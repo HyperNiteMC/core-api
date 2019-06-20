860626773ca65a5a8b9b4f4446b3dd34c7925a4e
Lam <tsukiko852@gmail.com>
2019-06-20 16:07:58 +0800

    change HttpRequest return type to CompletableFuture

--- API/src/com/hypernite/mc/hnmc/core/utils/HttpRequest.java
+++ API/src/com/hypernite/mc/hnmc/core/utils/HttpRequest.java
@@ -5,25 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 /**
  * HttpRequest 工具
  */
 public class HttpRequest {
 
-    public static String get(String link) throws IOException {
-        URL url = new URL(link);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        return response.toString();
+    public static CompletableFuture<String> get(String link) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                URL url = new URL(link);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("User-Agent", "Mozilla/5.0");
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            } catch (IOException e) {
+                throw new CompletionException(e);
+            }
+        });
     }
 }
