9fff4af0ebb2e8fe549bb7abd466b46d653f2cb8
eric2788 <tsukiko852@gmail.com>
2020-02-20 16:43:30 +0800

    FileController now throw IOException

--- API/src/com/hypernite/mc/hnmc/core/config/FileController.java
+++ API/src/com/hypernite/mc/hnmc/core/config/FileController.java
@@ -2,9 +2,11 @@ package com.hypernite.mc.hnmc.core.config;
 
 import com.hypernite.mc.hnmc.core.config.yaml.Configuration;
 
+import java.io.IOException;
+
 public interface FileController {
 
-    <T extends Configuration> void save(T config);
+    <T extends Configuration> void save(T config) throws IOException;
 
     <T extends Configuration> void reload(T config);
 
